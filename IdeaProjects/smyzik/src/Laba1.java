a96c6b309f496081c888cd432e0fa3c3e0b63ddc
SmyZ1k <zmkllkmk123pro@gmail.com>
2025-09-25 17:20:26 +0300

    1

--- IdeaProjects/smyzik/src/Laba1.java
+++ IdeaProjects/smyzik/src/Laba1.java
@@ -6,21 +6,21 @@ public class Laba1 {
     public static void main(String[] args) {
         int[] w = new int[((UPPER_BRACKET1 - LOWER_BRACKET1) / 2) + 1];
         float[] x = new float[20];
-        float[][] k = new float[9][20];
+        float[][] l = new float[9][20];
         int pointer = 0;
-        for (int l = UPPER_BRACKET1; l >= 5; l -= 2) {
-            w[pointer] = l;
+        for (int k = UPPER_BRACKET1; k >= 5; k -= 2) {
+            w[pointer] = k;
             pointer += 1;
         }
-        for(int l = 0; l<x.length;l++) {
-            x[l] = (float) (Math.random() * (UPPER_BRACKET2-LOWER_BRACKET2) + LOWER_BRACKET2);
+        for(int k = 0; k<x.length;k++) {
+            x[k] = (float) (Math.random() * (UPPER_BRACKET2-LOWER_BRACKET2) + LOWER_BRACKET2);
         }
         for(int i =0;i<9;i++){
             for(int j=0;j<20;j++){
-                k[i][j]=calc(w[i],x[j]);
+                l[i][j]=calc(w[i],x[j]);
             }
         }
-        print(k);
+        print(l);
 
     }
     public static float calc(int w,float x){
