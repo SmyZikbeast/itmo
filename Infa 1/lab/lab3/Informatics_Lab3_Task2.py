443399862762972f0f1bf57e38a8f8ff7fed4c3d
SmyZ1k <zmkllkmk123pro@gmail.com>
2025-10-31 16:53:01 +0300

    1

--- Infa 1/lab/lab3/Informatics_Lab3_Task2.py
+++ Infa 1/lab/lab3/Informatics_Lab3_Task2.py
@@ -6,10 +6,14 @@ from re import *
 
 def func(x):
     return ' h'+str(5*int(x)**3-13)+' '
+
 isu = 502792
 print('var number:', str(isu%5))
+
 integer = r'(([ ]|^)([1-9][0-9]*)([ ]|$))'
 example='12354376 + 645.34556 = 234 31254 412'
+
+example = sub(integer, lambda m: func(m.group()), example)
 example = sub(integer, lambda m: func(m.group()), example)
 
 print(example.strip().replace('h',''))
