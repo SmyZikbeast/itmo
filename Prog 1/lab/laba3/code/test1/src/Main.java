a0382502b72b6b86c092683c9be820d0344088d1
SmyZ1k <zmkllkmk123pro@gmail.com>
2025-12-24 13:29:17 +0300

    1

--- Prog 1/lab/laba3/code/test1/src/Main.java
+++ Prog 1/lab/laba3/code/test1/src/Main.java
@@ -1,20 +1,5 @@
-import java.io.IOException;
-
 class Main {
      public static void main(String[] args) {
-         a.b();
-         c.d();
+
      }
- }
- class a {
-    public static void b() throws ArrayIndexOutOfBoundsException{
-        int[] nums = {1, 2, 3};
-        System.out.println(nums[5]);
-    }
 }
- class c {
-     public static void d() throws IOException {
-         int[] nums = {1, 2, 3};
-         System.out.println(nums[5]);
-     }
- }
